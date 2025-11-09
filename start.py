0a1f96217e92a4960cbb64d0fc57d1d374687a83
Евгений <137326840+Belkub@users.noreply.github.com>
2025-11-09 13:57:35 +0100

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title = "This is a Multipage WebApp")
+st.title("HDD technological applications")
+st.write("Пакет приложений, предназначенных для оценки качества бентопорошка и расчета объемов бурового раствора, его рецептуры и расхода реагентов")
+st.sidebar.success("Выбрать приложение")
