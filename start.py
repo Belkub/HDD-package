a735b8ef48a271aad4f52580fe57bf815d0ffe2d
Евгений <137326840+Belkub@users.noreply.github.com>
2025-11-09 17:06:08 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.set_page_config(page_title = "This is a Multipage WebApp")
+st.title('Bentonite Co  &  Albrehta Co')
 st.title("HDD technological applications")
 st.write("Пакет приложений, предназначенных для оценки качества бентопорошка и расчета объемов бурового раствора, его рецептуры и расхода реагентов")
 st.sidebar.success("Выбрать приложение")
+
